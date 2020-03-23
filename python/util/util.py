a495503106feaccf76e8039558f115c413b51893
Mori Ohara <ohara@jp.ibm.com>
2020-03-23 10:15:54 +0900

    Cleaning up util.py

--- python/util/util.py
+++ python/util/util.py
@@ -41,29 +41,18 @@ class Channel:
             self._credentials = credentials
             self._access_token = ''
             self._expiration = int(time.time())
-            # print('initial expiration=' + str(self._expiration))
     
         def __call__(self, context, callback):
             #print('__call__ context=' + str(context))
             current = int(time.time())
             expiration = int(self._expiration) - 60 # set the expiration 60 sec before the actual one
-            # print('remaining=' + str(expiration - current) + ' expiration=' + str(expiration) + ' current=' + str(current))
             if expiration < current:
-                # print('renewing an access token')
                 self.get_access_token()
-                valid_for = int(self._expiration) - int(time.time())
-                # print('new expiration=' + str(self._expiration) + ' valid for ' + str(valid_for) + ' sec')
+
             metadata = (('authorization', 'Bearer {}'.format(self._access_token)),('bluemix-instance', '{}'.format(self._credentials['Instance'])),)
-            #print('metadata=' + str(metadata))
             callback(metadata, None)
 
         def get_access_token(self):
-            # print("*** get a new access token for an HPCS instance on IBM Cloud ***")
-        
-            # print("APIKEY=" + self._credentials['APIKey'])
-            # print("INSTANCE_ID=" + self._credentials['Instance'])
-            # print("ENDPOINT=" + self._credentials['Endpoint'])
-
             apikey = self._credentials['APIKey']
             endpoint = self._credentials['Endpoint']
             cmd = 'curl -sS -k -X POST --header "Content-Type: application/x-www-form-urlencoded" --header "Accept: application/json" --data-urlencode "grant_type=urn:ibm:params:oauth:grant-type:apikey" --data-urlencode "apikey=' + apikey + '" "' + endpoint + '/identity/token"'
@@ -79,10 +68,8 @@ class Channel:
 
             try:
                 resp = json.loads(resp_str)
-                # print('response=' + json.dumps(resp, indent=4))
                 self._expiration = resp['expiration']
                 self._access_token = resp['access_token']
-                # print('access_token=' + 'xxxxxxxxxxxxxxxxxxxxxxxxx...')
                 return self._access_token
         
             except Exception as e:
